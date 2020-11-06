770b8932c8261b5aa1e9a488845d7723f27f89ef
noman13bd <noman13bd@gmail.com>
2020-11-06 07:16:36 +0600

    updated

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
 from flask import Flask, jsonify, request
 import jwt
 import datetime
+from functools import wraps
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'MyJWTDemo'
+#postgresql://USER:PASS@HOST:PORT/DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/postgres"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80))
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.name = name
+        self.email = email
+    
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
+    def verify_password(self, pwd):
+        return check_password_hash(self.password, pwd)
 
 def check_if_authorized(func):
     ''' decorator to check if token is provided '''
+    @wraps(func)
     def executor(*args, **kwargs):
-        print(request.args)
-        token = request.args.get('token')
+        token = None
+        if request.headers.has_key('Access-token'):
+            token = request.headers['Access-Token']
         if not token:
             return jsonify({'message':'Missing Token'}), 403
         try:
@@ -31,7 +67,7 @@ def login():
         if data['un'] and data['pwd'] == '123456':
             token = jwt.encode({
                 'user': data['un'],
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
             }, app.config['SECRET_KEY'])
             return jsonify({'token': token.decode('utf-8')})
         else:
